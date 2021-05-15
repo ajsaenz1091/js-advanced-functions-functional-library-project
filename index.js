5d74670506c166fc7d32da74389c75a826f7b72d
Alberto <ajsaenz1091@gmail.com>
2021-05-15 19:43:22 -0400

    commit tree b6e33455278d6e0559a523e5d403eda062e9a9fe

--- index.js
+++ index.js
@@ -4,17 +4,61 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
+    each: function(collection, callback) {
+        if(Array.isArray(collection)){
+          for(let item of collection){
+            callback(item)
+          }
+        }else{
+          for(let key in collection){
+            callback(collection[key])
+          }
+        }
+        return collection
+    },
+
+    map: function(collection, callback) {
+      let newCollection = []
+      if(Array.isArray(collection)){
+        for(let item of collection){
+          newCollection.push(callback(item))
+        }
+      }else{
+        for(let key in collection){
+          newCollection.push(callback(collection[key])) 
+        }
+      }
+      return newCollection
+    },
+
+    reduce: function(collection,callback,acc=0) {
+      let reduced = 0;
+      for(let item of collection){
+        reduced += callback(acc,item,collection)
+      }
+      return reduced;
+    },
+
+    find: function(collection, predicate) {
+      foundItem;
+      for(let item of collection){
+        if(predicate(item) === true){
+          foundItem = item;
+          break;
+        }
+      }
+      return foundItem;
+    },
+
+    filter: function() {
 
     },
 
-    map: function() {
+    size: function() {
 
     },
 
-    reduce: function() {
 
-    },
 
     functions: function() {
 
